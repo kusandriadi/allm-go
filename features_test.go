15405090b4ce126f77adee49a3a0e04355c2711c
Kus Andriadi <me@kusandriadi.com>
2026-03-22 20:25:10 +0700

    fix: update version test to match 0.8.7

--- features_test.go
+++ features_test.go
@@ -441,7 +441,7 @@ func TestErrNotSupported(t *testing.T) {
 
 // TestVersion tests that version was updated
 func TestVersionUpdated(t *testing.T) {
-	if Version != "0.8.3" {
-		t.Errorf("Version = %s, want 0.8.3", Version)
+	if Version != "0.8.7" {
+		t.Errorf("Version = %s, want 0.8.7", Version)
 	}
 }
